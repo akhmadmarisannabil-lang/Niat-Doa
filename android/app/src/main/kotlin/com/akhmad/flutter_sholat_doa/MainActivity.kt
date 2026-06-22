ce92afcd84b76f027ef80d447743a8ab25f81743
aliimron <akhmadmarisannabil@gmail.com>
2026-06-22 21:12:40 +0700

    tahap akhir

---
+++ android/app/src/main/kotlin/com/akhmad/flutter_sholat_doa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.akhmad.flutter_sholat_doa  
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/flutter_sholat_doa/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_sholat_doa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
